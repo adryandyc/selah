c1fcd4e799dbfc5dec722fadaecf4a521bc57a2b
Adryandy Chandra <adryandyc@gmail.com>
2025-05-19 14:39:48 +0700

    Initial commit of Selah project

---
+++ Selah/SelahApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelahApp.swift
+//  Selah
+//
+//  Created by Adryandy on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SelahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SelahTests/SelahTests.swift
@@ -0,0 +1,17 @@
+//
+//  SelahTests.swift
+//  SelahTests
+//
+//  Created by Adryandy on 19/05/25.
+//
+
+import Testing
+@testable import Selah
+
+struct SelahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
